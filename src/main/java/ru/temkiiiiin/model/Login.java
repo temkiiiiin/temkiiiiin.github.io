a848631c440ae3131abe97bd15c7487d0261fa0e
temkiiiiin <temkiiiiin@gmail.com>
2018-02-23 12:51:35 +0300

    борьба с реквестами

--- src/main/java/ru/temkiiiiin/model/Login.java
+++ src/main/java/ru/temkiiiiin/model/Login.java
@@ -35,7 +35,7 @@ public class Login extends HttpServlet {
             request.setAttribute("errorString", errorString);
             //response.getWriter().print(errorString + " " + userName + " " + password);
 
-            RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/login.jsp");
+            RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("login.jsp");
             dispatcher.forward(request, response);
         } else {
             HttpSession session = request.getSession();
